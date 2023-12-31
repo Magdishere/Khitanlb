18ff1f27e32ba5a17aae4b4f3aaaaa5fbc23a2e1
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2023-12-31 03:28:51 +0200

    login

--- resources/views/Back/auth/login.blade.php
+++ resources/views/Back/auth/login.blade.php
@@ -26,14 +26,15 @@
                 <div class="card col-lg-4 mx-auto">
                     <div class="card-body px-5 py-5">
                         <h3 class="card-title text-left mb-3">Login</h3>
-                        <form>
+                        <form action="{{route('admin.login')}}" method="POST">
+                            @csrf
                             <div class="form-group">
-                                <label>Username or email *</label>
-                                <input type="text" class="form-control p_input">
+                                <label>email *</label>
+                                <input name="email" type="email" class="form-control p_input">
                             </div>
                             <div class="form-group">
                                 <label>Password *</label>
-                                <input type="text" class="form-control p_input">
+                                <input name="password" type="password" class="form-control p_input">
                             </div>
                             <div class="form-group d-flex align-items-center justify-content-between">
                                 <div class="form-check">
