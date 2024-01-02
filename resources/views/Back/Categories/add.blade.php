2894abeef5a096d239fd1a9360fe28671812f8e4
Magd <magdelzalameh6@gmail.com>
2024-01-02 12:08:59 +0200

    Categories

--- resources/views/Back/Categories/add.blade.php
+++ resources/views/Back/Categories/add.blade.php
@@ -9,15 +9,16 @@
 @section('page-header')
     <div class="main-panel">
         <div class="content-wrapper">
-            <div class="page-header">
-                <h3 class="page-title"> Add Category </h3>
-                <nav aria-label="breadcrumb">
-                    <ol class="breadcrumb">
-                        <li class="breadcrumb-item"><a href="#">Categories</a></li>
-                        <li class="breadcrumb-item active" aria-current="page">Add Category</li>
-                    </ol>
-                </nav>
-            </div>
+            				<!-- breadcrumb -->
+				<div class="breadcrumb-header justify-content-between">
+					<div class="my-auto">
+						<div class="d-flex">
+							<h4 class="content-title mb-0 my-auto">Add Category </h4><span class="text-muted mt-1 tx-13 mr-2 mb-0"> / Categories</span>
+						</div>
+					</div>
+				</div>
+				<!-- breadcrumb -->
+
             <div class="row">
                 <div class="col-12 grid-margin">
                     <div class="card">
--- resources/views/Back/auth/register.blade.php
+++ resources/views/Back/auth/register.blade.php
@@ -6,17 +6,17 @@
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <title>Corona Admin</title>
     <!-- plugins:css -->
-    <link rel="stylesheet" href="vendors/mdi/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="vendors/css/vendor.bundle.base.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/mdi/css/materialdesignicons.min.css')}}">
+    <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.base.css')}}">
     <!-- endinject -->
     <!-- Plugin css for this page -->
     <!-- End plugin css for this page -->
     <!-- inject:css -->
     <!-- endinject -->
     <!-- Layout styles -->
-    <link rel="stylesheet" href="css/style.css')}}">
+    <link rel="stylesheet" href="{{asset('css/style.css')}}">
     <!-- End layout styles -->
-    <link rel="shortcut icon" href="images/favicon.png')}}" />
+    <link rel="shortcut icon" href="{{asset('images/favicon.png')}}" />
 </head>
 <body>
 <div class="container-scroller">
@@ -80,16 +80,16 @@
 
 <!-- container-scroller -->
 <!-- plugins:js -->
-<script src="vendors/js/vendor.bundle.base.js')}}"></script>
+<script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
 <!-- endinject -->
 <!-- Plugin js for this page -->
 <!-- End plugin js for this page -->
 <!-- inject:js -->
-<script src="js/off-canvas.js')}}"></script>
-<script src="js/hoverable-collapse.js')}}"></script>
-<script src="js/misc.js')}}"></script>
-<script src="js/settings.js')}}"></script>
-<script src="js/todolist.js')}}"></script>
+<script src="{{asset('js/off-canvas.js')}}"></script>
+<script src="{{asset('js/hoverable-collapse.js')}}"></script>
+<script src="{{asset('js/misc.js')}}"></script>
+<script src="{{asset('js/settings.js')}}"></script>
+<script src="{{asset('js/todolist.js')}}"></script>
 <!-- endinject -->
 </body>
 </html>
