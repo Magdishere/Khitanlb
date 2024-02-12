b3482bdd0974a3693737e6f5ec39c0397c3b3eed
Aya Hosny <96696906+ayahosnii@users.noreply.github.com>
2024-02-12 18:41:53 +0200

    login

--- resources/views/Back/auth/login.blade.php
+++ resources/views/Back/auth/login.blade.php
@@ -1,86 +1,62 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Khitan</title>
-    <!-- plugins:css -->
-    <link rel="stylesheet" href="{{asset('vendors/mdi/css/materialdesignicons.min.css')}}">
-    <link rel="stylesheet" href="{{asset('vendors/css/vendor.bundle.base.css')}}">
-    <!-- endinject -->
-    <!-- Plugin css for this page -->
-    <!-- End plugin css for this page -->
-    <!-- inject:css -->
-    <!-- endinject -->
-    <!-- Layout styles -->
-    <link rel="stylesheet" href="{{asset('admin-assets/css/style.css')}}">
-    <!-- End layout styles -->
-    <link rel="shortcut icon" href="{{asset('admin-assets/images/favicon.png')}}" />
-</head>
-<body>
-<div class="container-scroller">
-    <div class="container-fluid page-body-wrapper full-page-wrapper">
-        <div class="row w-100 m-0">
-            <div class="content-wrapper full-page-wrapper d-flex align-items-center auth login-bg">
-                <div class="card col-lg-4 mx-auto">
-                    <div class="card-body px-5 py-5">
-                        <h3 class="card-title text-left mb-3">Login</h3>
-                        <form action="{{ route('admin.login') }}" method="POST">
-                            @csrf
-                            <div class="form-group">
-                                <label>Email *</label>
-                                <input name="email" type="email" class="form-control p_input">
-                                @error('email')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="form-group">
-                                <label>Password *</label>
-                                <input name="password" type="password" class="form-control p_input">
-                                @error('password')
-                                <span class="text-danger">{{ $message }}</span>
-                                @enderror
-                            </div>
-                            <div class="form-group d-flex align-items-center justify-content-between">
-                                <div class="form-check">
-                                    <label class="form-check-label">
-                                        <input type="checkbox" class="form-check-input"> Remember me </label>
-                                </div>
-                                <a href="#" class="forgot-pass">Forgot password</a>
-                            </div>
-                            <div class="text-center">
-                                <button type="submit" class="btn btn-primary btn-block enter-btn">Login</button>
-                            </div>
-                            <div class="d-flex">
-                                <button class="btn btn-facebook mr-2 col">
-                                    <i class="mdi mdi-facebook"></i> Facebook </button>
-                                <button class="btn btn-google col">
-                                    <i class="mdi mdi-google-plus"></i> Google plus </button>
-                            </div>
-                            <p class="sign-up">Don't have an Account?<a href="#"> Sign Up</a></p>
-                        </form>
+@extends('layouts.master2')
+@section('css')
+    <!-- Sidemenu-respoansive-tabs css -->
+    <link href="{{URL::asset('admin-assets/plugins/sidemenu-responsive-tabs/css/sidemenu-responsive-tabs.css')}}" rel="stylesheet">
+@endsection
+@section('content')
+    <div class="container-fluid">
+        <div class="row no-gutter">
+            <!-- The image half -->
+            <div class="col-md-6 col-lg-6 col-xl-7 d-none d-md-flex bg-primary-transparent">
+                <div class="row wd-100p mx-auto text-center">
+                    <div class="col-md-12 col-lg-12 col-xl-12 my-auto mx-auto wd-100p">
+                        <img src="{{URL::asset('admin-assets/img/media/login.png')}}" class="my-auto ht-xl-80p wd-md-100p wd-xl-80p mx-auto" alt="logo">
                     </div>
                 </div>
             </div>
-            <!-- content-wrapper ends -->
+            <!-- The content half -->
+            <div class="col-md-6 col-lg-6 col-xl-5 bg-white">
+                <div class="login d-flex align-items-center py-2">
+                    <!-- Demo content-->
+                    <div class="container p-0">
+                        <div class="row">
+                            <div class="col-md-10 col-lg-10 col-xl-9 mx-auto">
+                                <div class="card-sigin">
+                                    <div class="mb-5 d-flex"> <a href="{{ url('/' . $page='index') }}"><img src="{{URL::asset('admin-assets/img/brand/favicon.png')}}" class="sign-favicon ht-40" alt="logo"></a><h1 class="main-logo1 ml-1 mr-0 my-auto tx-28">Va<span>le</span>x</h1></div>
+                                    <div class="card-sigin">
+                                        <div class="main-signup-header">
+                                            <h2>Welcome back!</h2>
+                                            <h5 class="font-weight-semibold mb-4">Please sign in to continue.</h5>
+                                            <form action="#">
+                                                <div class="form-group">
+                                                    <label>Email</label> <input class="form-control" placeholder="Enter your email" type="text">
+                                                </div>
+                                                <div class="form-group">
+                                                    <label>Password</label> <input class="form-control" placeholder="Enter your password" type="password">
+                                                </div><button class="btn btn-main-primary btn-block">Sign In</button>
+                                                <div class="row row-xs">
+                                                    <div class="col-sm-6">
+                                                        <button class="btn btn-block"><i class="fab fa-facebook-f"></i> Signup with Facebook</button>
+                                                    </div>
+                                                    <div class="col-sm-6 mg-t-10 mg-sm-t-0">
+                                                        <button class="btn btn-info btn-block"><i class="fab fa-twitter"></i> Signup with Twitter</button>
+                                                    </div>
+                                                </div>
+                                            </form>
+                                            <div class="main-signin-footer mt-5">
+                                                <p><a href="">Forgot password?</a></p>
+                                                <p>Don't have an account? <a href="{{ url('/' . $page='signup') }}">Create an Account</a></p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    </div><!-- End -->
+                </div>
+            </div><!-- End -->
         </div>
-        <!-- row ends -->
     </div>
-    <!-- page-body-wrapper ends -->
-</div>
-<!-- container-scroller -->
-<!-- plugins:js -->
-<script src="{{asset('vendors/js/vendor.bundle.base.js')}}"></script>
-<!-- endinject -->
-<!-- Plugin js for this page -->
-<!-- End plugin js for this page -->
-<!-- inject:js -->
-<script src="{{asset('admin-assets/js/off-canvas.js')}}"></script>
-<script src="{{asset('admin-assets/js/hoverable-collapse.js')}}"></script>
-<script src="{{asset('admin-assets/js/misc.js')}}"></script>
-<script src="{{asset('admin-assets/js/settings.js')}}"></script>
-<script src="{{asset('admin-assets/js/todolist.js')}}"></script>
-<!-- endinject -->
-</body>
-</html>
+@endsection
+@section('js')
+@endsection
