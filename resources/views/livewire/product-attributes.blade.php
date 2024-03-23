ef4d1def187f41865cbdefb03dd3213b63e6005d
Aya Hosny <aya.hosni@id3m.com>
2024-03-23 18:51:33 +0200

    Attribute

--- resources/views/livewire/product-attributes.blade.php
+++ resources/views/livewire/product-attributes.blade.php
@@ -1,7 +1,7 @@
 <div>
 
 @if (App\Sale\Sale::calculateDiscountedPrice($product['id']) != '-')
-    <h3>${{ App\Sale\Sale::calculateDiscountedPrice($product['id'])}} <span>{{ $calculatedPrice ? $calculatedPrice : $price }}</span></h3>
+    <h3>${{ App\Sale\Sale::calculateDiscountedPrice($product['id'])}} <span>{{ $calculatedPrice }}</span></h3>
 @else
     <h3>${{$calculatedPrice ? $calculatedPrice : $product->regular_price}}</h3>
 @endif
