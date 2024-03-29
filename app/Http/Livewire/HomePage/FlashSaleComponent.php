941f184106ca0f73b338b1fc3ade4a880395a9de
Aya Hosny <aya.hosni@id3m.com>
2024-03-29 14:50:00 +0200

    Flashsale

--- app/Http/Livewire/HomePage/FlashSaleComponent.php
+++ app/Http/Livewire/HomePage/FlashSaleComponent.php
@@ -2,11 +2,22 @@
 
 namespace App\Http\Livewire\HomePage;
 
+use App\Http\Livewire\BaseComponent;
+use App\Models\admin\Product;
 use Livewire\Component;
 use App\Models\Sale;
 
-class FlashSaleComponent extends Component
+class FlashSaleComponent extends BaseComponent
 {
+    public function mount()
+    {
+        // Initialize selectedColors for each product
+        $products = Product::all();
+        foreach ($products as $product) {
+            $this->selectedColors[$product->id] = null;
+        }
+
+    }
     public function render()
     {
 
